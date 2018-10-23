3c4405f8731d67f48e12ec2f0fa0092b3a934314
Ruka7 <andrea.tagliavini.94@gmail.com>
2018-10-23 14:38:28 +0200

    chiamata a python funzionante

--- gephi-plugins-statistic/modules/MyStatistic/src/main/java/org/gephi/plugins/example/statistics/CountSelfLoop.java
+++ gephi-plugins-statistic/modules/MyStatistic/src/main/java/org/gephi/plugins/example/statistics/CountSelfLoop.java
@@ -1,46 +1,39 @@
 package org.gephi.plugins.example.statistics;
 
-import org.gephi.graph.api.Edge;
-import org.gephi.graph.api.Graph;
 import org.gephi.graph.api.GraphModel;
 import org.gephi.statistics.spi.Statistics;
 
 import java.io.IOException;
-
 public class CountSelfLoop implements Statistics {
 
     //Result
 
     @Override
     public void execute(GraphModel graphModel) {
-        Thread z = new Thread(){
-            public void run(){
-                ProcessBuilder pb = new ProcessBuilder("python", "prova.py");
-                try {
-                    Process p = pb.start();
-                }catch(IOException e){
-                    e.printStackTrace();
-                }
-            }
-        };
-        z.start();
 
+        try {
+            Process p = Runtime.getRuntime().exec("py C:\\Users\\andre\\OneDrive\\Documenti\\GitHub\\ProgettoIA\\gephi-plugins-statistic\\modules\\MyStatistic\\src\\main\\java\\org\\gephi\\plugins\\example\\statistics\\prova.py");
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
     @Override
     public String getReport() {
-        String report = "<HTML> <BODY> <h1>Embedding Report </h1> "
+        return "<HTML> <BODY> <h1>Embedding Report </h1> "
                 + "<hr>"
                 + "<br> <h2> Results: </h2>"
                 + "Total number of edges: "
                 + "<br />Number of self-loop: "
                 + "<br />"
                 + "</BODY></HTML>";
-        return report;
     }
 
-    public int getSelfLoopCount() {
+    int getSelfLoopCount() {
         return 0;
     }
 }
--- gephi-plugins-statistic/modules/MyStatistic/src/main/java/org/gephi/plugins/example/statistics/prova.py
+++ gephi-plugins-statistic/modules/MyStatistic/src/main/java/org/gephi/plugins/example/statistics/prova.py
@@ -1,4 +1,5 @@
-x = 1
-if x == 1:
-    # indented four spaces
-    print("x is 1.")
+data_path = "..\\..\\modules\\MyStatistic\\src\\main\\java\\org\\gephi\\plugins\\example\\statistics\\data\\"
+
+file = open(data_path + "test.txt", "w")
+file.write("Va tutto paul alla faccia tua")
+file.close()
